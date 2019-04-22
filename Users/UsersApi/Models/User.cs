5124f0d1b0a86f6c5956fe0baee49fd1883891cc
Raul HM <hmraul@gmail.com>
2019-04-22 16:15:20 +0200

    Adding LiteDB reference

---
+++ Users/UsersApi/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UsersApi.Models
+{
+    public class User
+    {
+        private string _username;
+        private List<string> _roles;
+        private string _password;
+
+        public User(string username, List<string> roles, string password)
+        {
+            _username = username;
+            _roles = roles;
+            _password = password;
+        }
+    }
+}
